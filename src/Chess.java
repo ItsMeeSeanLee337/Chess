9799a557545618a23cfc09d4a76d964be6fef9fe
Sean Lee <slee206061@gmail.com>
2023-03-12 20:08:30 -0400

    Updated binary files

--- src/Chess.java
+++ src/Chess.java
@@ -11,7 +11,6 @@ public class Chess {
         {" a", " b", " c", " d", " e", " f", " g", " h"},
     };
 
-    
     public static void main(String[] args) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
