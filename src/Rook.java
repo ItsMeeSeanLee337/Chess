fd45e76cf2f4b34d81cf6e2bdd0ef81bf0cc2cad
Sean Lee <slee206061@gmail.com>
2023-03-14 18:42:29 -0400

    Updated Rook class with isValidMove method implemented

--- src/Rook.java
+++ src/Rook.java
@@ -2,7 +2,7 @@ public class Rook extends Piece{
     private boolean hasMoved;
     
     public Rook(String color, int rank, int file) {
-        super(color, color == "White" ? "wR" : "bR", rank, file);
+        super(color, color.equals("White") ? "wR" : "bR", rank, file);
         this.hasMoved = false;
     }
 
@@ -25,6 +25,55 @@ public class Rook extends Piece{
 
     public boolean isValidMove(int toRank, int toFile, Board board) 
     {
-        
+        // check if the move is within the board
+        if (toRank < 0 || toRank > 7 || toFile < 0 || toFile > 7) 
+        {
+            return false;
+        }
+
+        Piece destPiece = board.getPiece(toRank, toFile); // check if the destination square is empty
+
+        // check if the rook is moving vertically
+        if (toFile == getFile()) 
+        {
+            // check if there are any pieces blocking the rook's path
+            int start = Math.min(getRank(), toRank);
+            int end = Math.max(getRank(), toRank);
+            for (int i = start + 1; i < end; i++) 
+            {
+                Piece p = board.getPiece(i, getFile());
+                if (p != null) 
+                {
+                    return false;
+                }
+            }
+            // check if the destination square is empty or contains an opponent's piece
+            if (destPiece == null || !destPiece.getColor().equals(getColor())) 
+            {
+                return true;
+            }
+        }
+        // check if the rook is moving horizontally
+        else if (toRank == getRank()) 
+        {
+            // check if there are any pieces blocking the rook's path
+            int start = Math.min(getFile(), toFile);
+            int end = Math.max(getFile(), toFile);
+            for (int i = start + 1; i < end; i++) 
+            {
+                Piece p = board.getPiece(getRank(), i);
+                if (p != null) 
+                {
+                    return false;
+                }
+            }
+            // check if the destination square is empty or contains an opponent's piece
+            if (destPiece == null || !destPiece.getColor().equals(getColor())) 
+            {
+                return true;
+            }
+        }
+        return false;
     }
+
 }
