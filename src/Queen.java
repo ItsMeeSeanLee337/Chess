3fd5b0cf26e5b60ff2a86209d74ef367afdeeae0
Sean Lee <slee206061@gmail.com>
2023-03-14 18:51:52 -0400

    Implemented isValidMove method in Queen class

--- src/Queen.java
+++ src/Queen.java
@@ -23,22 +23,31 @@ public class Queen extends Piece{
         return hasMoved;
     }
 
-    public boolean isValidMove(int toRank, int toFile, Board board) 
-    {
-        // check if the move is within the board
-        if (toRank < 0 || toRank > 7 || toFile < 0 || toFile > 7) 
-        {
-            return false;
-        }
-
-        Piece destPiece = board.getPiece(toRank, toFile); // check if the destination square is empty
-        if (getColor().equals("White")) // White pawn
-        {
-                
-        }
-        else // Black pawn
-        {
-
-        }
+    public boolean isValidMove(int toRank, int toFile, Board board) {
+    // check if the move is within the board
+    if (toRank < 0 || toRank > 7 || toFile < 0 || toFile > 7) {
+        return false;
     }
+
+    Piece destPiece = board.getPiece(toRank, toFile); // check if the destination square is empty
+    if (destPiece != null && destPiece.getColor().equals(getColor())) {
+        return false;
+    }
+
+    // check if the move is along a straight line (like a rook)
+    if (getRank() == toRank || getFile() == toFile) {
+        Rook rook = new Rook(getColor(), getRank(), getFile());
+        return rook.isValidMove(toRank, toFile, board);
+    }
+
+    // check if the move is along a diagonal line (like a bishop)
+    if (Math.abs(toRank - getRank()) == Math.abs(toFile - getFile())) {
+        Bishop bishop = new Bishop(getColor(), getRank(), getFile());
+        return bishop.isValidMove(toRank, toFile, board);
+    }
+
+    // if neither of the above, the move is not valid
+    return false;
+}
+    
 }
