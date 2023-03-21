5c1b8810c7378dfa61bb61ab13595eeccd091eb0
Sean Lee <slee206061@gmail.com>
2023-03-21 18:24:33 -0400

    Updated bugs list

--- src/Game.java
+++ src/Game.java
@@ -117,7 +117,7 @@ public class Game
  * 8. Need to check if checkmate is prpoerly identified and properly ends the game
  * 9. Input c8 h3 gives index out of bounds error, this is for moving a black bishop from it's starting position to h3, I suspect this bug is for bishops specifically
  * 11. Rook is able to pass through pieces, need to check if this is a bug with rook specifically or applies to other pieces
- * 13. Input c8 g4 gives index out of bounds error, this is for moving a black bishop from it's starting position to g4
- * 14. Queen is able to pass through pieces if moving horizontally or vertically, not diagonally, byproduct of using rooks methods in the queen file
- * 15. Queen cannot make the move d6 a3, should be possible if queen is on square d6, possibly a byproduct of using bishop methods in the queen file
+ * 12. Input c8 g4 gives index out of bounds error, this is for moving a black bishop from it's starting position to g4
+ * 13. Queen is able to pass through pieces if moving horizontally or vertically, not diagonally, byproduct of using rooks methods in the queen file
+ * 14. Queen cannot make the move d6 a3, should be possible if queen is on square d6, possibly a byproduct of using bishop methods in the queen file
  */
